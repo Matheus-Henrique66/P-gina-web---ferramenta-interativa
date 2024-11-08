d1d74dd3172eaefb2851f48bddf5aba16d32bcbd
Matheus-Henrique66 <matheushenriquetimao123@gmail.com>
2024-11-08 19:49:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criacartao(
+    'Programação',
+    'O que é python',
+    'O python é uma linguagem de programação'
+)
+
+criacartao(
+    'Geogafria',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criacartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função na programação é um bloco de código que executa alguma coisa'
+)
+
+criacartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
